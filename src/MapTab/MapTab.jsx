b04696cd3398e448d9209ef01a41dbdcbdfb833d
Andrea Benedetti <benedetti.a93@gmail.com>
2026-07-07 16:49:52 +0200

    Update MapTab.jsx

--- src/MapTab/MapTab.jsx
+++ src/MapTab/MapTab.jsx
@@ -14,13 +14,13 @@ function MapTab({ data }) {
           value="list"
           className={"text-mauve-700 h-full px-5 py-2 hover:cursor-pointer"}
         >
-          List
+          Lista
         </Tabs.Tab>
         <Tabs.Tab
           value="map"
           className={"text-mauve-700 h-full px-5 py-2 hover:cursor-pointer"}
         >
-          Map
+          Mappa
         </Tabs.Tab>
         <Tabs.Indicator className="absolute top-0 left-0 -z-1 h-full w-(--active-tab-width) translate-x-(--active-tab-left) border-b-mauve-600 border-b-4 transition-[translate,width] duration-150 ease-in-out" />
       </Tabs.List>
