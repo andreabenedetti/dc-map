77e663b1bc00408606e7e27a674b90a0950b6453
Andrea Benedetti <benedetti.a93@gmail.com>
2026-07-13 13:26:27 +0200

    text

--- src/App.jsx
+++ src/App.jsx
@@ -36,9 +36,10 @@ function App() {
       </div>
       <Intro />
       {/* <MapTab data={data} />*/}
-      <div className={"container mx-auto px-4 sm:px-0 py-20 flex flex-wrap items-center justify-center space-y-4"}>
-        <img src="img-2.png" className="w-120 h-auto max-w-full" />
-      <p className={"font-serif text-lg text-slate-600 text-balance text-center"}>Presto pubblicheremo i dati che abbiamo raccolto. Seguici su @n.i.n.a_watch per ricevere aggiornamenti e informazioni aggiuntive.</p></div>
+      <div className={"container mx-auto px-4 sm:px-0 py-20 items-center justify-center space-y-2"}>
+        <img src="img-2.png" className="w-90 h-auto max-w-full mx-auto" />
+        <p className={"font-serif text-2xl text-slate-600 text-balance text-center"}>Presto pubblicheremo i dati che abbiamo raccolto</p>
+        <p className={"text-sans text-base text-slate-600 text-balance text-center"}>Seguici su @n.i.n.a_watch per ricevere aggiornamenti e informazioni aggiuntive.</p></div>
       <Footer />
     </>
   );
--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -1,16 +1,31 @@
 function Intro() {
   return (
     <div className="w-full bg-indigo-200">
-      <div className="container px-4 sm:px-0 mx-auto py-14 font-serif text-xl hyphens-auto sm:flex gap-6 items-center">
-        <div className="w-full sm:w-2/3">
-          <p className="mb-6 text-2xl text-indigo-950">
+      <div className="container px-4 sm:px-0 mx-auto py-14 text-lg hyphens-auto sm:flex gap-6 items-center space-y-4">
+        <div className="w-full sm:w-2/3 space-y-4">
+          <p className="mb-6 text-2xl text-indigo-950 font-serif">
             "Fortezze AI" raccoglie i progetti in via di valutazione e
             costruzione dei nuovi data center nei dintorni di Milano.
             Questi data center hanno svariati usi, ma nascono
             prevalentemente dalla necessità di computazione per favorire
             l'adozione di modelli di intelligenza artificiale.
           </p>
-          <h3 className="text-3xl mt-7 mb-4">Come abbiamo raccolto i dati</h3>
+          <h3 className="text-3xl mt-7 mb-4 font-serif">Perché una mappatura?</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <h3 className="text-3xl mt-7 mb-4 font-serif">Come attivarsi?</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="w-full sm:w-1/3">
+          <img src="cover-3.png" className="w-fit" />
+        </div>
+      </div>
+      <div className="container px-4 sm:px-0 mx-auto py-14 text-lg hyphens-auto sm:flex gap-6 items-center">
+        <div className="w-full sm:w-2/3 space-y-4">
+          <h3 className="text-3xl mt-7 mb-4 font-serif">Come abbiamo raccolto i dati</h3>
           <p>
             Per costruire questa mappatura, stiamo raccogliendo tutti i
             progetti pubblicamente disponibili sul sito del Ministero
@@ -25,9 +40,14 @@ function Intro() {
             possibile trovare una trentina di progetti attualmente in
             valutazione, in opera o in valutazione di ottemperanza.
           </p>
-        </div>
-        <div className="w-full sm:w-1/3">
-          <img src="cover-3.png" className="w-fit" />
+          <p>
+            I progetti presentano molti documenti di descrizione del progetto, dagli studi di fattibilità, agli impatti ambientali, fino alle planimetrie dei data centers. Abbiamo ridisegnato le aree geografiche interessate su Google Earth e compilato tutte le aree in un unico file geografico che ci permettesse di avere una visione d'insieme.
+          </p>
+          <p>Siamo interessatə ad approfondire:</p>
+          <ul className="list-decimal">
+            <li>I dettagli delle aziende che propongono i progetti di costruzione dei data center, dalla loro provenienza ai dettagli economici che le sostengono;</li>
+            <li>La potenza energetica che i progetti richiedono di avere, e come verrà soddisfatto questo fabbisogno;</li>
+            <li>Come la popolazione può intervenire, partecipare e dissentire nei processi decisionali di approvazione dei progetti.</li></ul>
         </div>
       </div>
     </div>
