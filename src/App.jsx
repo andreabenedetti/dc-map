a4b88020110da9eb0eea420498c88667874af91e
Andrea Benedetti <benedetti.a93@gmail.com>
2026-07-14 10:38:35 +0200

    content

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import MapTab from "./MapTab/MapTab";
 import InfoDrawer from "./InfoDrawer/InfoDrawer";
 import Intro from "./Intro/Intro";
 
+import { EnvelopeIcon } from "@phosphor-icons/react/dist/ssr";
+
 function App() {
   const data = [...list].sort((a, b) => (a.proposer > b.proposer ? 1 : -1));
 
   return (
     <>
-      <div className={"text-blue-400 bg-indigo-950 w-full pb-5 sm:h-2/3"}>
+      <div className={"text-white bg-indigo-950 w-full pb-5 sm:h-2/3"}>
         <div className={"container mx-auto px-4 sm:px-0 py-4"}>
           <div className={"flex justify-between align-middle"}>
             <p>NINA</p>
@@ -30,16 +32,20 @@ function App() {
             Fortezze A.I.
           </h1>
           <h2 className={"w-full text-3xl sm:text-4xl font-serif text-balance"}>
-            Come i data center stanno diventando le nuove fortezze dell'intelligenza artificiale
+            La nuvola è fatta di terra: mappiamo l'infrastruttura dell'estrattivismo digitale
           </h2>
         </div>
       </div>
       <Intro />
       {/* <MapTab data={data} />*/}
-      <div className={"container mx-auto px-4 sm:px-0 py-20 items-center justify-center space-y-2"}>
+      <div className={"container mx-auto w-full sm:w-4xl px-4 sm:px-0 py-20 items-center justify-center space-y-3"}>
         <img src="img-2.png" className="w-70 h-auto max-w-full mx-auto" />
-        <p className={"font-serif text-2xl text-slate-600 text-balance text-center"}>Presto pubblicheremo i dati che abbiamo raccolto</p>
-        <p className={"text-sans text-base text-slate-600 text-balance text-center"}>Seguici su @n.i.n.a_watch per ricevere aggiornamenti e informazioni aggiuntive.</p></div>
+        <p className={"font-serif text-2xl text-slate-600 text-center"}>Partecipa alla mappatura</p>
+        <p className={"text-sans text-base text-slate-600 text-center"}>Così come N.i.n.a. non è un'isola, ma un'infrastruttura a disposizione di chi lotta, questa mappatura è pensata per essere utilizzata da chiunque ne abbia bisogno.</p>
+        <p className={"text-sans text-base text-slate-600 text-center"}>Se fai parte di un collettivo ecologista, di un sindacato, di un gruppo di ricerca studentesco, o se sei semplicemente un cittadino o una cittadina che vuole opporsi a questa colonizzazione tecnologica sul proprio territorio, vogliamo unire le forze con te. Tutto può essere bene comune, dai dati ai territori, e solo collettivamente possiamo difenderli.</p>
+        <div className={"w-fit mx-auto flex items-center gap-2 bg-indigo-950 px-4 py-2 text-white transition-all hover:bg-blue-200 hover:text-blue-500"}><EnvelopeIcon size={18} /> <a href="mailto:">Contattaci via mail</a></div>
+
+      </div>
       <Footer />
     </>
   );
--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -3,21 +3,14 @@ function Intro() {
     <div className="w-full bg-indigo-200">
       <div className="container px-4 sm:px-0 mx-auto py-14 text-lg hyphens-auto sm:flex gap-6 items-center space-y-4">
         <div className="w-full sm:w-2/3 space-y-4">
-          <p className="mb-6 text-2xl text-indigo-950 font-serif">
-            "Fortezze AI" raccoglie i progetti in via di valutazione e
-            costruzione dei nuovi data center nei dintorni di Milano.
-            Questi data center hanno svariati usi, ma nascono
-            prevalentemente dalla necessità di computazione per favorire
-            l'adozione di modelli di intelligenza artificiale.
+          <p>
+            L'Intelligenza Artificiale ci viene venduta come una magia immateriale, una “nuvola” neutra e priva di peso. Questa è l'illusione fondante del tecno-capitalismo. Ma la realtà è ben diversa: l'IA è un'infrastruttura fisica, pesante ed estremamente vorace. È una catena di montaggio globale che consuma suolo, prosciuga riserve idriche e divora energia. E il modo in cui verrà sviluppata in questi mesi plasmerà la società in cui vivremo per decenni.</p>
+          <p>
+            Come dimostrano le prime storiche manifestazioni di questi giorni contro l'insediamento dei data center in Lombardia – vere e proprie opere di colonizzazione territoriale che consumano quanto 500.000 famiglie, restituendo in cambio pochissima occupazione e zero know-how locale – il conflitto non è più solo digitale. L'estrattivismo algoritmico ha un impatto materiale e violento sui nostri territori, sull'ambiente e sulle risorse comuni.
           </p>
           <h3 className="text-3xl mt-7 mb-4 font-serif">Perché una mappatura?</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <h3 className="text-3xl mt-7 mb-4 font-serif">Come attivarsi?</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <p>Il tempo della diagnosi è finito. Non si può negoziare con un potere che rimane invisibile. Come N.i.n.a. (Né Intelligente Né Artificiale), stiamo conducendo una mappatura indipendente dei sistemi estrattivi e dei data center sul territorio: vogliamo comprendere come si sviluppa l'intelligenza artificiale e l'infrastruttura tecnologica alla sua base, per capire come poterle rendere contendibili e collettive.</p>
+          <p>Mappare l'hardware del tecno-capitalismo, i suoi consumi e le sue reti significa infatti fornire alla società civile, ai sindacati e ai movimenti per la giustizia climatica le coordinate esatte per difendersi, organizzare il dissenso e immaginare alternative di sovranità tecnologica dal basso.</p>
         </div>
         <div className="w-full sm:w-1/3">
           <img src="cover-3.png" className="w-fit" />
@@ -27,24 +20,17 @@ function Intro() {
         <div className="w-full sm:w-2/3 space-y-4">
           <h3 className="text-3xl mt-7 mb-4 font-serif">Come abbiamo raccolto i dati</h3>
           <p>
-            Per costruire questa mappatura, stiamo raccogliendo tutti i
-            progetti pubblicamente disponibili sul sito del Ministero
-            dell'Ambiente e della Sicurezza Energetica, letto i documenti
-            e estrapolato le informazioni sulle aree del territorio che
-            verranno impattate da questi progetti.
+            Per costruire questa mappatura, stiamo raccogliendo tutti i progetti pubblicamente disponibili sul sito del <a href="https://va.mite.gov.it/it-IT/Ricerca/Via" target="">Ministero dell'Ambiente e della Sicurezza Energetica</a>, letto i documenti e estrapolato le informazioni sulle aree del territorio che verranno impattate da questi progetti.
           </p>
           <p>
-            Sul{" "}
-            <a href="https://va.mite.gov.it/it-IT/Ricerca/">portale</a>,
-            cercando con la parola chiave "data center" e "datacenter", è
-            possibile trovare una trentina di progetti attualmente in
-            valutazione, in opera o in valutazione di ottemperanza.
+            Sul portale, con la parola chiave “data center” e “datacenter”, vengono restituiti 44 progetti rilevanti in Lombardia: questi progetti riguardano <b>nuovi data center, espansioni di data center esistenti, la costruzione di gruppi energetici aggiuntivi, e la costruzione di pozzi per il trattamento delle acque.</b>
           </p>
           <p>
             I progetti presentano molti documenti di descrizione del progetto, dagli studi di fattibilità, agli impatti ambientali, fino alle planimetrie dei data centers. Abbiamo ridisegnato le aree geografiche interessate su Google Earth e compilato tutte le aree in un unico file geografico che ci permettesse di avere una visione d'insieme.
           </p>
           <p>Siamo interessatə ad approfondire:</p>
-          <ul className="list-decimal">
+          <ul className="list-disc list-inside">
+            <li>Altri casi non rappresentati sul sito del Ministero;</li>
             <li>I dettagli delle aziende che propongono i progetti di costruzione dei data center, dalla loro provenienza ai dettagli economici che le sostengono;</li>
             <li>La potenza energetica che i progetti richiedono di avere, e come verrà soddisfatto questo fabbisogno;</li>
             <li>Come la popolazione può intervenire, partecipare e dissentire nei processi decisionali di approvazione dei progetti.</li></ul>
